4ca3e3f8ea18b5665e8b4431760022b83aa4f06c
navinkumar4214 <navinkumar4214@gmail.com>
2026-06-24 09:52:37 +0530

    Implement books and members management

--- frontend/src/pages/Members.jsx
+++ frontend/src/pages/Members.jsx
@@ -1,7 +1,102 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Members() {
+  const [members, setMembers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    fetchMembers();
+  }, []);
+
+  const fetchMembers = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await axios.get(
+        "http://localhost:5000/api/members",
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      setMembers(res.data);
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addMember = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    await axios.post(
+      "http://localhost:5000/api/members",
+      {
+        name,
+        email,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    setName("");
+    setEmail("");
+
+    fetchMembers();
+
+  } catch (err) {
+    console.log(err);
+  }
+};
   return (
     <div>
       <h1>Members Page</h1>
+
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button onClick={addMember}>
+        Add Member
+      </button>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {members.map((member) => (
+            <tr key={member.id}>
+              <td>{member.id}</td>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
